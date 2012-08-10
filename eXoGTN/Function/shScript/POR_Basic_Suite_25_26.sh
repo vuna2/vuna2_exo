a19e1903df07eafb5bdf5b585c9ab522c2544b14
vuna2 <vuna2@exoplatform.com>
2012-08-10 13:48:17 +0700

    Update eXoGTN

---
+++ eXoGTN/Function/shScript/POR_Basic_Suite_25_26.sh
@@ -0,0 +1,9 @@
+#export HOME_TEST="/home/vuna2/java/exo-working/qa-data/qa-data-repository/DailyReports/PLF/selenium/src/suite/org/exoplatform/selenium/candidate/eXoGTN"
+echo "================exoGTN Basic Action====================="
+export HOME_TEST=/home/vuna2/java/exo-working/qa-data/qa-data-repository/DailyReports/PLF/selenium/src/suite/org/exoplatform/selenium/candidate/eXoGTN
+cd $HOME_TEST
+export ReportName=POR_Basic_Suite_25_26_$(date +"%m-%d-%Y").html
+pwd
+java -jar selenium-server-standalone-2.21.0.jar -userExtensions /home/vuna2/java/exo-working/test/eXo-automation/jsScript/user-extensions.js -htmlsuite "*firefox" "http://localhost:8080" $HOME_TEST/Function/BasicFunction/POR_Basic_Suite_25_26.html $HOME_TEST/Function/Reports/$ReportName
+exit 0
+
---
+++ eXoGTN/Function/shScript/eXoGTN_ApplicationRegistry.sh
@@ -0,0 +1,8 @@
+#export HOME_TEST="/home/vuna2/java/exo-working/qa-data/qa-data-repository/DailyReports/PLF/selenium/src/suite/org/exoplatform/selenium/candidate/eXoGTN"
+echo "================exoGTN Basic Action====================="
+export HOME_TEST=/home/vuna2/java/exo-working/qa-data/qa-data-repository/DailyReports/PLF/selenium/src/suite/org/exoplatform/selenium/candidate/eXoGTN
+cd $HOME_TEST
+export ReportName=eXoGTN_ApplicationRegistry_$(date +"%m-%d-%Y").html
+pwd
+java -jar selenium-server-standalone-2.21.0.jar -userExtensions /home/vuna2/java/exo-working/test/eXo-automation/jsScript/user-extensions.js -htmlsuite "*firefox" "http://localhost:8080" $HOME_TEST/Function/ApplicationRegistry/eXoGTN_ApplicationRegistry.html $HOME_TEST/Function/Reports/$ReportName
+exit 0
